b823b2dae88bdc7cdbc10d193c6607b373aa5e9a
SamuelMalaga <samuelmalaga99@gmail.com>
2022-08-23 16:33:43 -0300

    Finalização da estruturação da base do projeto no django com as models criadas juntamente com as views do admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from .models import Produto, Contato, Usuario, Endereco
+
 # Register your models here.
+
+class UsuarioAdmin(admin.ModelAdmin):
+  list_display = ['nome_usuario', 'sobrenome_usuario', 'email_usuario', 'data_nascimento_usuario', 'DOC_Usuario', 'is_CNPJ', 'is_Produtor', 'senha_usuario']
+  pass
+
+class ProdutoAdmin(admin.ModelAdmin):
+  list_display = ['nome_produto', 'preco_produto', 'descricao_produto','quantidade_produto','imagem_produto','tipo_produto']
+  pass
+
+class EnderecoAdmin(admin.ModelAdmin):
+  list_display = ['rua_endereco', 'CEP_endereco', 'bairro_endereco', 'cidade_endereco', 'estado_endereco']
+  pass
+
+class ContatoAdmin(admin.ModelAdmin):
+  list_display = ['telefone_contato', 'email_contato', 'is_wpp']
+  pass
+
+
+
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Endereco, EnderecoAdmin)
+admin.site.register(Contato, ContatoAdmin)
+
--- core/models.py
+++ core/models.py
@@ -12,14 +12,43 @@ import uuid
 #   tipo_produto = models.CharField('tipo_produto', max_length=200)
 #   produtor_id = models.ForeignKey('Produtor', on_delete=models.CASCADE)
 
-class Cliente(models.Model):
-  nome_cliente = models.CharField('nome_cliente', max_length=225)
-  sobrenome_cliente = models.CharField('sobrenome_cliente', max_length=225)
-  email_cliente = models.DecimalField('email_cliente', max_length=225)
-  data_nascimento_cliente = models.DateField('data_nascimento_cliente', blank=True, null=True )
-  CPF_cliente = models.CharField('CPF_cliente', max_length=225)
+class Usuario(models.Model):
+  nome_usuario = models.CharField('nome_usuario', max_length=225)
+  sobrenome_usuario = models.CharField('sobrenome_usuario', max_length=225)
+  email_usuario = models.CharField('email_usuario', max_length=225)
+  data_nascimento_usuario = models.DateField('data_nascimento_usuario', blank=True, null=True )
+  DOC_Usuario = models.CharField('DOC_Usuario', max_length=225)
+  is_CNPJ = models.BooleanField('is_CNPJ')
   is_Produtor = models.BooleanField('is_Produtor')
+  senha_usuario = models.CharField('senha_usuario', max_length=225)
+
+  def __str__(self) -> str:
+    return self.nome_usuario
+
+class Produto(models.Model):
+  nome_produto = models.CharField('nome_produto', max_length=225)
+  preco_produto = models.DecimalField('preco_produto', decimal_places=2, max_digits=15)
+  descricao_produto = models.CharField('descricao_produto',max_length=225, blank=True, null=True )
+  quantidade_produto = models.DecimalField('quantidade_produto',decimal_places=2, max_digits=15)
+  imagem_produto = models.ImageField('imagem_produto', blank=True, null=True, upload_to="core")
   tipo_produto = models.CharField('tipo_produto', max_length=200)
-  produtor_id = models.ForeignKey('Produtor', on_delete=models.CASCADE)
+  def __str__(self) -> str:
+    return self.nome_produto
 
+class Endereco(models.Model):
+  rua_endereco = models.CharField('rua_endereco', max_length=225)
+  CEP_endereco = models.CharField('CEP_endereco', max_length=225)
+  bairro_endereco = models.CharField('bairro_endereco', max_length=225)
+  cidade_endereco = models.CharField('cidade_endereco', max_length=225)
+  estado_endereco = models.CharField('estado_endereco', max_length=225)
 
+  #def __str__(self) -> str:
+  #  return self.nome_produto
+
+class Contato(models.Model):
+  telefone_contato = models.CharField('telefone_contato', max_length=225)
+  email_contato = models.CharField('email_contato', max_length=225)
+  is_wpp = models.BooleanField('is_wpp')
+
+  #def __str__(self) -> str:
+  #  return self.nome_produto
